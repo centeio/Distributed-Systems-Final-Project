8e156505eb1e34acd3b20953cb8a1660b6960865
centeio <c.centeiojorge@outlook.pt>
2017-05-12 15:29:45 +0100

    stupid error

--- src/Operator.java
+++ src/Operator.java
@@ -4,6 +4,20 @@ public class Operator implements Runnable{
 
 	public Operator(Client c) {
 		super();
+		this.setC(c);
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Client getC() {
+		return c;
+	}
+
+	public void setC(Client c) {
 		this.c = c;
 	}
 	
